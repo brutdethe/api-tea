41bfea4fbf699b091115c7de8b694cbb95615fb7
pntbr <pntbr@pm.me>
2020-05-02 18:26:48 +0200

    move teas file to json

--- index.mjs
+++ index.mjs
@@ -1,21 +1,21 @@
+import fs from 'fs'
 import express from 'express'
-import db from './teas.mjs'
 
 const app = express()
 app.get('/api/v1/teas', (req, res) => {
     res.status(200).send({
         success: 'true',
         message: 'teas retrieved successfully',
-        teas: db
+        teas: JSON.parse(fs.readFileSync('teas.json', 'utf8'))
     })
 })
 
 app.get('/', (req, res) => {
     res.status(200).send({
         success: 'true',
-        message: 'teas retrieved successfully',
-        teas: db
+        message: 'API-tea documentation',
+        api: JSON.parse(fs.readFileSync('api-tea.json', 'utf8'))
     })
 })
 
-app.listen(process.env.PORT || 3000, () => console.log("Server is running..."))
+app.listen(process.env.PORT || 3000, () => console.log("Server is running : http://localhost:3000"))
