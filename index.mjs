9886601101070c911c22d0fa0ecadf104a120ba4
pntbr <pntbr@pm.me>
2020-05-03 00:36:44 +0200

    add routes for json files

--- index.mjs
+++ index.mjs
@@ -2,6 +2,12 @@ import fs from 'fs'
 import express from 'express'
 
 const app = express()
+
+res.status(200).send({
+    xx        api: JSON.parse(fs.readFileSync('./data/json/api-tea.json', 'utf8'))
+})
+})
+
 app.get('/api/v1/teas', (req, res) => {
     res.status(200).send({
         success: 'true',
@@ -10,11 +16,67 @@ app.get('/api/v1/teas', (req, res) => {
     })
 })
 
-app.get('/', (req, res) => {
+app.get('/api/v1/types', (req, res) => {
     res.status(200).send({
         success: 'true',
-        message: 'API-tea documentation',
-        api: JSON.parse(fs.readFileSync('./data/json/api-tea.json', 'utf8'))
+        message: 'tea types retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/types.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/cultivars', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea cultivars retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/cultivars.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/families', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea families retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/families.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/picking', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea picking retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/picking.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/provinces', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea provinces retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/provinces.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/towns', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea towns retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/towns.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/brewing-types', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea brewing-types retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/brewing-types.json', 'utf8'))
+    })
+})
+
+app.get('/api/v1/harvests', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'tea harvests retrieved successfully',
+        teas: JSON.parse(fs.readFileSync('./data/json/harvests.json', 'utf8'))
     })
 })
 
