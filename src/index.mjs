c963ee5a96e50b7ced07323b698e221df38e020b
pntbr <pntbr@pm.me>
2020-05-03 20:27:52 +0200

    add routes dynamically

--- src/index.mjs
+++ src/index.mjs
@@ -1,109 +1,52 @@
 import fs from 'fs'
 import path from 'path'
 import express from 'express'
-import summary from '../src/summary'
 import getSummary from './summary.mjs'
+
+const endPoints = [
+    'teas',
+    'types',
+    'cultivars',
+    'families',
+    'picking',
+    'provinces',
+    'towns',
+    'brewing-types',
+    'harvests'
+]
 
 const app = express()
 
-const getJsonPath = name =>
-    path.format({
-        root: process.cwd(),
-        dir: 'data/json',
-        name: name,
-        ext: '.json'
-    })
+function AddEndPointsRoutes(endPoints, app) {
+    const getJsonPath = name =>
+        path.format({
+            root: process.cwd(),
+            dir: 'data/json',
+            name: name,
+            ext: '.json'
+        })
+
+    endPoints.forEach(endPoint =>
+        app.get(`/api/v1/${endPoint}`, (req, res) => {
+            res.status(200).send({
+                success: 'true',
+                message: `${endPoint} retrieved successfully`,
+                api: JSON.parse(fs.readFileSync(getJsonPath(endPoint), 'utf8'))
+            })
+        })
+    )
+}
 
 app.get('/', (req, res) => {
     const host = new URL('api/v1/', `${req.protocol}://${req.get('host')}/`)
     res.status(200).send({
         success: 'true',
         message: 'API-tea documentation',
-        api: getSummary(host, [
-            'teas',
-            'types',
-            'cultivars',
-            'families',
-            'picking',
-            'provinces',
-            'towns',
-            'brewing-types',
-            'harvests'
-        ])
+        api: getSummary(host, endPoints)
     })
 })
 
-app.get('/api/v1/teas', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'teas retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('teas'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/types', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea types retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('types'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/cultivars', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea cultivars retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('cultivars'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/families', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea families retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('families'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/picking', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea picking retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('picking'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/provinces', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea provinces retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('provinces'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/towns', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea towns retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('towns'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/brewing-types', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea brewing-types retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('brewing-types'), 'utf8'))
-    })
-})
-
-app.get('/api/v1/harvests', (req, res) => {
-    res.status(200).send({
-        success: 'true',
-        message: 'tea harvests retrieved successfully',
-        teas: JSON.parse(fs.readFileSync(getJsonPath('harvests'), 'utf8'))
-    })
-})
+AddEndPointsRoutes(endPoints, app)
 
 app.listen(process.env.PORT || 5000, function() {
     console.log('Server is running : http://localhost:5000')
